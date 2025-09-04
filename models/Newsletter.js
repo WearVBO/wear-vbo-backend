b57a94b2ac699ee77b4c8c6e6c5a8b4eb49b3bce
uchechukwu <uchechukwupeter22@gmail.com>
2025-09-04 10:00:09 +0100

    json file updated

--- models/Newsletter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const NewsletterSchema = new mongoose.Schema({
-  // fullname: { type: String, trim: true, required: true },
-  email: { type: String, trim: true, required: true },
-  subscribed: { type: Boolean, default: true },
-});
-
-module.exports = mongoose.model("newsletter", NewsletterSchema);
